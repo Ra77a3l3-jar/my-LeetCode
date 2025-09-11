87b211ce195827cbf024931c239adcb4a1042627
Ra77a3l3-jar <raffaelemeo77@gmail.com>
2025-09-11 21:28:16 +0200

    feat: longest common prefix
    - given an array of strings it finds the commond prefix if there is one
    - it takes the first string as base and then compares the other ones to
    it.
    - it changes the position of the null terminator in the prefix
    string/first string of the array since if the chars that got deleted
    were not in common even with a single string couln't be part of the
    prefix.

---
+++ Longest_Common_Prefix/long_common_prefix.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+char* longestCommonPrefix(char** strs, int strsSize) {
+    if (strsSize == 0) {
+        return "";
+    }
+
+    // First string is the common prefix
+    char* prefix = strs[0];
+
+    for (int i = 1; i < strsSize; i++) {
+        int j = 0;
+
+        // Compare the current string with the prefix character by character
+        // Stop if we reach the end of either string or find a mismatch
+        while (prefix[j] != '\0' && strs[i][j] != '\0' && prefix[j] == strs[i][j]) {
+            j++; // Move to the next character if they match
+        }
+
+        // At this point, `j` is the length of the common prefix between
+        // the current string and the prefix. Truncate the prefix here.
+        // If later strings have a shorter common prefix, this will shift left.
+        prefix[j] = '\0';
+    }
+
+    return prefix;
+}
+
+int main(void) {
+    char* strs[] = {"flower", "flow", "flight"};
+    int strsSize = sizeof(strs) / sizeof(strs[0]);
+
+    char* result = longestCommonPrefix(strs, strsSize);
+    printf("Longest Common Prefix: %s\n", result);
+
+    return 0;
+    
+}
