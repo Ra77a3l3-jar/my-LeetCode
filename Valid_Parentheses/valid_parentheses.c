cce7e904bd8d9f5d0f53d280833c11b070ab588a
Ra77a3l3-jar <raffaelemeo77@gmail.com>
2025-09-12 14:34:02 +0200

    Add Valid Parentheses algorithm with stack - To the stack i add each time if the parentheses is open - When i find a closing bracket but the bracket at the top of the stack is not the right one it returns FALSE - If the closing bracket finds a match with the top bracket of the stack it pops the top bracket

--- Valid_Parentheses/valid_parentheses.c
+++ Valid_Parentheses/valid_parentheses.c
@@ -9,14 +9,42 @@ bool isValid(char* s) {
         return false;
     }
 
+    char stack[256];
+    int topStack = -1;
+    int i = 0;
     
-    return true;
+    while(s[i] != '\n') {
+        if(s[i] == '(' || s[i] == '[' || s[i] == '{') {
+            topStack += 1;
+            stack[topStack] = s[i];
+            // If the found a opening bracket, it added to the top of the stack
+        } else {
+            if(topStack == -1) {
+                return false;
+            }
+
+            // Checks if the matching gave a negative result
+            if ((s[i] == ')' && stack[topStack] != '(') ||
+                (s[i] == ']' && stack[topStack] != '[') ||
+                (s[i] == '}' && stack[topStack] != '{')) {
+                return false;
+            }
+            topStack -= 1; // Popping the top char in case of a match
+        }
+    }
+    return topStack == -1; // Since if top stack is != -1 means there are still unmatched pairs
 }
 
 int main(void) {
 
     char parentheses[6] = "({[]})";
-    isValid(parentheses);
+    bool result = isValid(parentheses);
+
+    if(result) {
+        printf("The parentheses are valid.\n");
+    } else {
+        printf("The parentheses are not valid.\n");
+    }
 
     return 0;
 }
