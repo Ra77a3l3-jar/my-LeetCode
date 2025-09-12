1f6925a2c0c231a5523018550eca36856f7bd460
Ra77a3l3-jar <raffaelemeo77@gmail.com>
2025-09-12 13:23:52 +0200

    Update code comments

--- Longest_Common_Prefix/long_common_prefix.c
+++ Longest_Common_Prefix/long_common_prefix.c
@@ -5,7 +5,6 @@ char* longestCommonPrefix(char** strs, int strsSize) {
         return "";
     }
 
-    // First string is the common prefix
     char* prefix = strs[0];
 
     for (int i = 1; i < strsSize; i++) {
@@ -14,12 +13,12 @@ char* longestCommonPrefix(char** strs, int strsSize) {
         // Compare the current string with the prefix character by character
         // Stop if we reach the end of either string or find a mismatch
         while (prefix[j] != '\0' && strs[i][j] != '\0' && prefix[j] == strs[i][j]) {
-            j++; // Move to the next character if they match
+            j++; // Go to next character if they match
         }
 
-        // At this point, `j` is the length of the common prefix between
-        // the current string and the prefix. Truncate the prefix here.
-        // If later strings have a shorter common prefix, this will shift left.
+        /* At this point, `j` is the length of the common prefix between
+           the current string and the prefix. Truncate the prefix here.
+           If later strings have a shorter common prefix, this will shift left. */
         prefix[j] = '\0';
     }
 
