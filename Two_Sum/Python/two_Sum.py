dadd4332066cbf03160c0e5207753139ca0b3f46
Ra77a3l3-jar <raffaelemeo77@gmail.com>
2025-09-13 15:26:30 +0200

    Add Two Sum Python solution

---
+++ Two_Sum/Python/two_Sum.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        # Your solution logic goes here.
+        pass
+
+if __name__ == "__main__":
+    solution = Solution()
+    nums = [2, 7, 11, 15]
+    target = 9
+    result = solution.twoSum(nums, target)
+    print(result)  # Expected output: [0, 1]
