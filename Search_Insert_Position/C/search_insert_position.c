370bdc0876c52a61c80e2bb3e043d7d962f69c34
Ra77a3l3-jar <raffaelemeo77@gmail.com>
2025-09-16 17:33:30 +0200

    feat: search insert position logic compleate but 0(n) not 0(log n)

--- Search_Insert_Position/C/search_insert_position.c
+++ Search_Insert_Position/C/search_insert_position.c
@@ -1,14 +1,19 @@
 #include <stdio.h>
 
 int searchInsert(int* nums, int numsSize, int target) {
-    
+    for (int i = 0; i < numsSize; i++) {
+        if (nums[i] >= target) {
+            return i;
+        }
+    }
+    return numsSize;
 }
 
 int main(void) {
 
     int nums[4] = {1,3,5,6};
     int numsSize = sizeof(nums) / sizeof(nums[0]);
-    int target = 5;
+    int target = 7;
 
     int result = searchInsert(nums, numsSize, target);
     printf("The index in which the target is or would be present is: %d\n", result);
