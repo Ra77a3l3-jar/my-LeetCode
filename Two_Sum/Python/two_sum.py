5921c3c028b0aa315912942da2f6b783713076c8
Ra77a3l3-jar <raffaelemeo77@gmail.com>
2025-10-11 21:08:25 +0200

    feat: new version of two sum
    - Now i use a hash map(dictionarie) to store all the seen numbers
    - If the diffrence between target and num is in seen it returns num and
    the seen number
    - Else if saves in seen the number I
    - If no sum gives back target it returns an empty list

--- Two_Sum/Python/two_sum.py
+++ Two_Sum/Python/two_sum.py
@@ -1,21 +1,9 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-
-        for i in range(len(nums)):
-            for j in range(i + 1, len(nums)):
-                    if nums[i] + nums[j] == target:
-                        return [i, j]
-        
-        pass
-
-if __name__ == "__main__":
-    solution = Solution()
-    nums = [2, 7, 11, 15]
-    target = 9
-    result = solution.twoSum(nums, target)
-    print(result)
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        seen = {}
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in seen:
+                return [seen[complement], i]
+            seen[num] = i # Saves in seen number i
+        return [] # Returns empty list
