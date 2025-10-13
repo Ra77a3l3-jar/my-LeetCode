24687b0511b5954603fdd0f3aa06b93585f6af24
Ra77a3l3-jar <raffaelemeo77@gmail.com>
2025-10-13 13:20:04 +0200

    feat: new version to solve group of anagrams problem
    - In this version it creates an array with 26 slots where each start at
    0
    - The it cycles through each char in the string and calculates the
    frequency of each char for
    - And appends strings s to the dictionarie res using track as key
    - Track has to be casted into a tuple to be used as a key for a hash map

--- GroupAnagrams (49)/Python/GroupAnagrams.py
+++ GroupAnagrams (49)/Python/GroupAnagrams.py
@@ -4,7 +4,9 @@ class Solution:
         res = defaultdict(list) # Hash map di list vuote {key:[], key:[], key:[]}
 
         for s in strs:
-            key = ''.join(sorted(s))
-            res[key].append(s) # Based on the key it places the element in a given list
-
+            track = [0] * 26 # Array for frequency of each char in a str used as key
+            for c in s:
+                track[ord(c) = orc('a')] += 1 # ord(c)-ord('a') = index for given char
+            res[tuple(track)].append(s) # Appens str s to list with key track. Casted track into a tuple since keys have to be immutable
+            
         return list(res.values())
