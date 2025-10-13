eedf8e2fd7889ec9848b66e0cec6cb4e45927d87
Ra77a3l3-jar <raffaelemeo77@gmail.com>
2025-10-13 12:53:04 +0200

    feat: completed group anagrams problem
    - Given a list of strings it has to return a list of list in each list
    of list have to be grouped the anagrams.
    - It created a dictionarie of empty lists
    - Then it loops over each string in strs and generates a key by sorting
    the string "ate":key="aet ,"tea":key="aet"
    - And it places the string in the list with the same key as the one
    generated for the string
    - After finishing the loop it returns the dictionarie but with only
    values and no keys
    - Complexity of (n * l log l) n(number of strings) and l(avr lenght of
    each sorted string)

--- GroupAnagrams (49)/Python/GroupAnagrams.py
+++ GroupAnagrams (49)/Python/GroupAnagrams.py
@@ -1,3 +1,10 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
- 
+
+        res = defaultdict(list) # Hash map di list vuote {key:[], key:[], key:[]}
+
+        for s in strs:
+            key = ''.join(sorted(s))
+            res[key].append(s) # Based on the key it places the element in a given list
+
+        return list(res.values())
