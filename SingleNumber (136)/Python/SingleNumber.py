07e5f796cf8fe98314ae23601880e198bfdeb6f5
Ra77a3l3-jar <raffaelemeo77@gmail.com>
2025-10-14 14:19:38 +0200

    feat: completed the single number problem
    - The request is that given a list of numbers we are assured that there
    is a single element that appers only once
    - I used a very similar algorithm as in majority element
    - I save the value as a key in a dictionarie and i use the value to keep
    track of the frequency
    - Then i check for a value that has frequency 1, when it finds the value
    == 1 it returns the key

---
+++ SingleNumber (136)/Python/SingleNumber.py
@@ -0,0 +1,13 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        freq = {}
+
+        for i in nums:
+            if i in freq:
+                freq[i] += 1
+            else:
+                freq[i] = 1
+
+        for key in freq:
+            if freq[key] == 1:
+                return key
