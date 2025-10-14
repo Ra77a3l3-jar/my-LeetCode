a8867908fdd0905f0a77c6a3845371c543122e8b
Ra77a3l3-jar <raffaelemeo77@gmail.com>
2025-10-14 13:41:19 +0200

    Add two numbers problem - The problem uses linked lists - First it calculates the total value of the reversed value in the linked list the first value will have multiplier 1 the second will have multiplier 10 ecc. This proces repeats for both linked lists - Then it calculates the sum between the values of both list. - To repackage the result as a reversed linked list it applies the same proces to find the non reversed num but the other way.

--- AddTwoNumbers (2)/Python/AddTwoNumbers.py
+++ AddTwoNumbers (2)/Python/AddTwoNumbers.py
@@ -1,19 +1,33 @@
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        num1 = 0
+        num2 = 0
+        multiplier = 1
 
-        num1: int = 0
-        num2: int = 0
+        ptrL1 = l1
+        while ptrL1:
+            num1 += ptrL1.val * multiplier
+            multiplier *= 10
+            ptrL1 = ptrL1.next
 
-        for i in range(len(l1)):
-            num1 += l1[i] * (10 ** i)
+        multiplier = 1
+        ptrL2 = l2
+        while ptrL2:
+            num2 += ptrL2.val * multiplier
+            multiplier *= 10
+            ptrL2 = ptrL2.next
 
-        for j in range(len(l2)):
-            num2 += l2[j] * (10 ** j)
+        result = num1 + num2
 
-        result: int = num1 + num2
+        dummy = ListNode(0) # Initialize new list
+        curr = dummy
 
-        returnList = []
-        for i in range(len(result) -1, -1, -1):
-            returnList.append(i)
+        if result == 0:
+            return ListNode(0)
 
-        return returnList
+        while result > 0:
+            curr.next = ListNode(result % 10) # (result % 10) will be the value saved in the node
+            result //= 10 # Gives the back the 80 if result is 807 while result % 10 gives back 7
+            curr = curr.next
+
+        return dummy.next
